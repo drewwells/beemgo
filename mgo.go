f1b0ee07945f512ba558158d1011a0cf55a06c1e
Drew Wells <drew.wells00@gmail.com>
2014-07-02 02:07:40 -0500

    Manage connection pool limit, block when non available

--- mgo.go
+++ mgo.go
@@ -6,31 +6,53 @@ import (
 	"labix.org/v2/mgo"
 )
 
-type Mongo struct {
-	Session *mgo.Session
+var max = 10
+
+type Service struct {
+	spark *mgo.Session
+	queue chan int
+	Url   string
+	Open  int
+	pool  []*mgo.Session
 }
 
-var (
-	m = Mongo{}
-)
-
-func Dial(url string) (*mgo.Session, error) {
-	session, err := mgo.Dial(url)
-	session.SetMode(mgo.Monotonic, true)
-	m.Session = session
-	return session, err
-}
-
-//Return a copy of the original mongo connection parameters
-func Session() *mgo.Session {
-	if m.Session == nil {
-		log.Fatal("Session doesn't exist")
+// New bootstraps the Mongo pool making it possible to open
+// sessions.
+func (s *Service) New() {
+	var err error
+	s.queue = make(chan int, max)
+	for i := 0; i < max; i = i + 1 {
+		s.queue <- i
 	}
-	return m.Session.New()
+	s.pool = make([]*mgo.Session, max)
+	s.Open = 0
+	s.spark, err = mgo.Dial(s.Url)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
-func Close(s *mgo.Session) {
-	defer func() {
-		s.Close()
+// Session attempts to create a session in the pool.
+func (s *Service) Session() chan *mgo.Session {
+	ch := make(chan *mgo.Session)
+	go func() {
+		<-s.queue
+		s.Open++
+		sess := s.spark.Clone()
+		for i, v := range s.pool {
+			if v == nil {
+				s.pool[i] = sess
+				break
+			}
+		}
+		ch <- sess
 	}()
+	return ch
+}
+
+// Close an open session to free up space in the pool.
+func (s *Service) Close(m *mgo.Session) {
+	m.Close()
+	s.queue <- 1
+	s.Open--
 }
--- mgo_test.go
+++ mgo_test.go
@@ -15,17 +15,30 @@ type MgoSuite struct {
 }
 
 func (s *MgoSuite) TestNew(c *C) {
-	var err error
-	//Actually need a live mongo server to test this
-	s.sess, err = Dial("localhost")
-	if err != nil {
-		c.Log(err.Error())
+	svc := Service{
+		Url: "localhost",
 	}
-	s1 := s.sess
-	s2 := Session()
-	c.Assert(s2, FitsTypeOf, &mgo.Session{})
-	c.Assert(s1, Not(Equals), s2)
-}
+	svc.New()
+	ch := svc.Session()
+	s1 := <-ch
+	c.Assert(svc.Open, Equals, 1)
+	svc.Close(s1)
+	qSize := 0
+	for _, v := range svc.pool {
+		if v != nil {
+			qSize++
+		}
+	}
+	c.Assert(qSize, Equals, 1)
+	c.Assert(svc.Open, Equals, 0)
+	// Actually need a live mongo server to test this
+	// s.sess, err = Dial("localhost")
+	// if err != nil {
+	// 	c.Log(err.Error())
+	// }
+	// s1 := s.sess
+	// s2 := Session()
+	// c.Assert(s2, FitsTypeOf, &mgo.Session{})
+	// c.Assert(s1, Not(Equals), s2)
 
-func (s *MgoSuite) TestSession(c *C) {
 }
